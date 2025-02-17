b6e7711f4d915de69678def9902be08fa790a381
DoraMorais <isadora2008c@gmail.com>
2025-02-17 16:31:39 -0300

    modificação código index

---
+++ button-I.js
@@ -0,0 +1,14 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const instructionsBtn = document.getElementById('instructions-btn');
+    const instructionsMenu = document.getElementById('instructions-menu');
+    const closeBtn = document.getElementById('close-btn');
+
+    instructionsBtn.addEventListener('click', function () {
+        instructionsMenu.style.display = 'block';
+    });
+
+    closeBtn.addEventListener('click', function () {
+        instructionsMenu.style.display = 'none';
+    });
+});
