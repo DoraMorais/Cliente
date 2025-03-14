afa6ec2dae145e361ffa8e4810a02ca624c68f80
Isadora_Morais <isadora2008c@gmail.comm>
2025-03-13 21:11:04 -0300

    correção no botão de instruções

--- INSTbutton.js
+++ INSTbutton.js
@@ -3,14 +3,18 @@ document.addEventListener("DOMContentLoaded", function () {
     const instructionsMenu = document.getElementById('instructions-menu');
     const closeBtn = document.getElementById('close-btn');
 
-    instructionsBtn.addEventListener('click', function () {
-        instructionsMenu.style.display = 'block';
-    });
+    if (instructionsBtn && instructionsMenu && closeBtn) {
+        instructionsBtn.addEventListener('click', function () {
+            instructionsMenu.style.display = 'block';
+        });
 
-    closeBtn.addEventListener('click', function () {
-        instructionsMenu.style.display = 'none';
-    });
- });
+        closeBtn.addEventListener('click', function () {
+            instructionsMenu.style.display = 'none';
+        });
+    } else {
+        console.error('One or more elements not found');
+    }
+});
 
   // esse é o botão de instruções do formulário//
 //o botão exibe um menu de instruções quando clicado//
